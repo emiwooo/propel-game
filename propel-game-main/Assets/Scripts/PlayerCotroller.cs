d280ccfea17623705a8a33f8ace011605f9eb08c
NyktStrix <mdirga2006@gmail.com>
2026-06-24 10:17:55 +0200

    shoot and thrust fix

--- propel-game-main/Assets/Scripts/PlayerCotroller.cs
+++ propel-game-main/Assets/Scripts/PlayerCotroller.cs
@@ -16,9 +16,15 @@ public class PlayerCotroller : MonoBehaviour
     public InputAction MoveAction;
     public InputAction LookAction;
     public InputAction ThrustAction;
+    public InputAction ShootAction;
+    public float thrustOn = 0f;
     public float thrustAllow = 5;
     public float mouseOn = 0;
     public float thrustTracker = 0;
+    public float lineTracker = 0;
+    Vector2 cursorOffset = new Vector2(168f, 203f);
+
+    
    
 
 
@@ -30,6 +36,7 @@ public class PlayerCotroller : MonoBehaviour
         MoveAction.Enable();
         LookAction.Enable();
         ThrustAction.Enable();
+        ShootAction.Enable();
         
 
     }
@@ -40,15 +47,21 @@ public class PlayerCotroller : MonoBehaviour
     {
 
         
-        Vector2 playerMovement = MoveAction.ReadValue<Vector2>()*0.1f;
+        Vector2 playerMovement = MoveAction.ReadValue<Vector2>()*0.3f;
         Vector2 playerPosition = (Vector2)transform.position + playerMovement + playerVelocity;
-        Vector2 playerCursor = LookAction.ReadValue<Vector2>();
-        mouseOn = ThrustAction.ReadValue<float>();
-        playerAcceleration = new Vector2(0.0f, -0.01f); 
-       
+        Vector2 playerCursor = LookAction.ReadValue<Vector2>() - cursorOffset;
+        mouseOn = ShootAction.ReadValue<float>();
+        thrustOn = ThrustAction.ReadValue<float>();
+        playerAcceleration = new Vector2(0.0f, -0.01f);
+        LineRenderer lineRenderer = GetComponent<LineRenderer>();
+        lineRenderer.startWidth = 4;
+        lineRenderer.startColor = Color.purple;
+        Vector2 lineOrigin = transform.position;
+        
 
 
-        if ((mouseOn == 1.0f) && (playerCursor.x > 145) && (playerCursor.x < 190) && (playerCursor.y > 167) && (playerCursor.y < 243) && (thrustAllow > 0) && (thrustTracker == 0))
+
+        if ((thrustOn == 1.0f) && (thrustAllow > 0) && (thrustTracker == 0))
         {
             playerAcceleration = new Vector2(0.0f, 5f);
             thrustAllow -= 1;
@@ -57,17 +70,19 @@ public class PlayerCotroller : MonoBehaviour
 
 
         }
-        else if (mouseOn == 0f)
+        else if (thrustOn == 0f)
         {
             thrustTracker = 0;
             playerAcceleration = new Vector2(0.0f, -0.1f);
 
         }
-        else if (mouseOn == 1f)
+        
+        if (mouseOn == 1.0f)
         {
-
-
-
+            
+            lineRenderer.SetPosition(0, lineOrigin);
+            lineRenderer.SetPosition(1, lineOrigin + playerCursor);
+            
         }
 
         realAcceleration += playerAcceleration;
@@ -81,8 +96,10 @@ public class PlayerCotroller : MonoBehaviour
         playerVelocity = playerVelocity + playerAcceleration;
         playerVelocity.y = Math.Clamp(playerVelocity.y, -0.05f, +0.1f);
         Debug.Log(playerCursor);
-        Debug.Log(mouseOn);
-        Debug.Log(thrustAllow);
+        //Debug.Log(mouseOn);
+        //Debug.Log(thrustAllow);
+        
+        
       
        
 
