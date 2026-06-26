0db4efbb829262c90b155681d0074798567f160f
emiwooo <emily.wu2@student.unimelb.edu.au>
2026-06-26 18:05:20 +1000

    plane prefab

---
+++ propel-game-main/Assets/Scripts/Enemies/Plane.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class Plane : Enemy
+{
+    protected override void Awake()
+    {
+        base.Awake();
+    }
+
+    protected override void Move()
+    {
+        base.Move();
+    }
+}
